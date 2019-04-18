353327dc31b633ceda3fbca11fb6fa7e3ccfab2d
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2019-04-18 19:32:04 -0300

    leiaute

--- aula-01/java/ex4.java
+++ aula-01/java/ex4.java
@@ -1,19 +1,19 @@
 package ex3;
 
 public class ex4 {
-	
-	public static int mod(int x, int y) {
-		
-		int s = x;
-		
-		while(s >= y) {
-			s = s-y;
-		}
-		
-		return s;
-	}
-	
-	public static void main(String args[]) {
-		System.out.println(mod(5,5));
-	}
+
+    public static int mod(int x, int y) {
+
+        int s = x;
+
+        while (s >= y) {
+            s = s - y;
+        }
+
+        return s;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(mod(5, 5));
+    }
 }
