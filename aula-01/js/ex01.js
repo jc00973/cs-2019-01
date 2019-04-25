4e9bdcf940502a72c3f17bfacfaced5e1ee20bca
kyriosdata <fabio@inf.ufg.br>
2019-04-25 11:40:33 -0300

    verificar se inteiro

--- aula-01/js/ex01.js
+++ aula-01/js/ex01.js
@@ -1,16 +1,20 @@
 function propriedade3025(n) {
-	if (typeof n != "number") {
-		throw new TypeError("numero esperado");
-	}
+    if (typeof n != "number") {
+        throw new TypeError("numero esperado");
+    }
 
-	if (n < 0 || n > 9999) {
-		throw new RangeError("numero fora da faixa");
-	}
+    if (n < 0 || n > 9999) {
+        throw new RangeError("numero fora da faixa");
+    }
 
-	const i = Math.floor(n/100);
-    const j = Math.floor(n%100);
-    
-  	return (((i+j)**2) == n);
+    if (n != Math.floor(n)) {
+        throw new TypeError("apenas valor inteiro");
+    }
+
+    const i = Math.floor(n / 100);
+    const j = n % 100;
+
+    return ((i + j) ** 2) == n;
 }
 
 
