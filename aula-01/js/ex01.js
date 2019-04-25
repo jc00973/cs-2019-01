33da6ce164f16530bdc5b74bcb1a870f825c0584
kyriosdata <fabio@inf.ufg.br>
2019-04-25 11:32:08 -0300

    testar parametros é obrigatorio

--- aula-01/js/ex01.js
+++ aula-01/js/ex01.js
@@ -1,5 +1,12 @@
 function propriedade3025(n) {
-    
+	if (typeof n != "number") {
+		throw new TypeError("numero esperado");
+	}
+
+	if (n < 0 || n > 9999) {
+		throw new RangeError("numero fora da faixa");
+	}
+
 	const i = Math.floor(n/100);
     const j = Math.floor(n%100);
     
