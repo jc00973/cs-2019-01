574e251017fe0ecb98eecdcc1084e37c8e749a65
kyriosdata <fabio@inf.ufg.br>
2019-04-25 11:29:29 -0300

    voce deve testar os parametros recebidos

--- aula-01/java/com/github/jc00973/cs/aula1/ex20.java
+++ aula-01/java/com/github/jc00973/cs/aula1/ex20.java
@@ -3,6 +3,16 @@ package com.github.jc00973.cs.aula1;
 public class ex20 {
 	
 	public static boolean cpf(int d[]) {
+		if (d == null || d.length != 11) {
+			throw new IllegalArgumentException("cpf invalido");
+		}
+
+		for (int digito : d) {
+			if (digito < 0 || digito > 9) {
+				throw new IllegalArgumentException("cpf invalido");
+			}
+		}
+
 		int j = d[1] + 2*d[2] + 3*d[3] + 4*d[4] + 5*d[5] + 6*d[6] + 7*d[7] + 8*d[8] + 9*d[9];
 		int k = d[2] + 2*d[3] + 3*d[4] + 4*d[5] + 5*d[6] + 6*d[7] + 7*d[8] + 8*d[9] + 9*d[10];
 		
