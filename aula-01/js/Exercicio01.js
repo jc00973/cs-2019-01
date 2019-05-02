850010d0577c866ba0b6b02092c674b5eb9b6b06
João Carlos Fonseca <fonseca.jc@live.com>
2019-05-02 20:59:34 -0300

    Packages adicionados.

--- aula-01/js/Exercicio01.js
+++ aula-01/js/Exercicio01.js
@@ -1,7 +1,7 @@
 function propriedade3025(n) {
     
-	const i = Math.floor(n/100);
-    const j = Math.floor(n%100);
+	var i = Math.floor(n/100);
+    var j = Math.floor(n%100);
     
   	return (((i+j)**2) == n);
 }
